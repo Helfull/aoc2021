d019d7956d8c9c95bbb689b674f59efa1b9756c7
Sebastian Kraft <sebastian.kraft@buschglatz.com>
2021-12-02 21:12:09 +0100

    shorten day1

--- day1/part1.py
+++ day1/part1.py
@@ -1,18 +1,4 @@
-f = open('input_day1', 'r');
+f = open('./day1/input_day1', 'r');
 
-lastMeasure = 0
-increases = 0
-
-for line in f:
-    currentMeasure = int(line)
-    if lastMeasure == 0:
-        lastMeasure = int(currentMeasure)
-        continue
-    if lastMeasure < currentMeasure:
-        print("Increase: " + str(currentMeasure - lastMeasure))
-        increases += 1
-    else:
-        print("Decrease: " + str(lastMeasure - currentMeasure))
-    lastMeasure = int(currentMeasure)
-
-print(increases)
+readings = [(int(l)) for l in f.readlines()];
+print(sum([(readings[index-1] < readings[index]) for index in range(len(readings))]));
--- day1/part2.py
+++ day1/part2.py
@@ -1,22 +1,8 @@
-f = open('input_day1', 'r');
+f = open('./day1/input_day1', 'r');
 
 def sumReadings(readings, index):
     return sum(readings[index:index+3]);
 
 readings = [(int(l)) for l in f.readlines()];
-
-"""
-loop the readings and sum the next three numbers
-"""
-lastGroupSum = 0;
-increases = 0
-for i in range(len(readings)):
-    currentGroupSum = sumReadings(readings, i)
-    if currentGroupSum > lastGroupSum & lastGroupSum != 0:
-        print("increase: " + str(currentGroupSum))
-        increases += 1
-    lastGroupSum = currentGroupSum
-
-print(increases)
-
+print(sum([(sumReadings(readings, i) < sumReadings(readings, i+1)) for i in range(len(readings)-2)]));
 
